aead064692a23b957f63e9851e58f7d27a391e3e
Alan Fu <alanfu@live.com>
2020-04-28 15:48:30 -0700

    Created Assignment 6

---
+++ Assignment6.js
@@ -0,0 +1,26 @@
+// Create Math functions that will console log the result
+
+// each function should take in 2 input, an 'x' and an 'y'
+// at the end of each function, output the result using console log
+
+function add(/* Fill in the input*/){
+
+}
+
+function minus(/* Fill in the input*/){
+
+}
+
+function multiply(/* Fill in the input*/){
+
+}
+
+function divide(/*Fill in the input*/){
+
+}
+
+// Test cases:
+// add(1,9); ---> 10
+// minus(5, 3); ---> 2
+// multiply(2,4); ---> 8
+// divide(10, 2); ---> 5
