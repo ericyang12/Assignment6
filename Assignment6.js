7d1ffead77f1c937145b35d05ddc440d39c1b047
ericyang12 <ericyang4009@gmail.com>
2020-04-28 20:20:21 -0400

    created functions for add,minus,multiply, and divide

--- Assignment6.js
+++ Assignment6.js
@@ -3,24 +3,28 @@
 // each function should take in 2 input, an 'x' and an 'y'
 // at the end of each function, output the result using console log
 
-function add(/* Fill in the input*/){
-
+function add(x,y){
+var addition = x+y;
+console.log(addition);
 }
 
-function minus(/* Fill in the input*/){
-
+function minus(x,y){
+var min = x-y;
+console.log(min);
 }
 
-function multiply(/* Fill in the input*/){
-
+function multiply(x,y){
+var mul = x*y;
+console.log(mul);
 }
 
-function divide(/*Fill in the input*/){
-
+function divide(x,y){
+var div = x/y;
+console.log(div);
 }
 
 // Test cases:
-// add(1,9); ---> 10
-// minus(5, 3); ---> 2
-// multiply(2,4); ---> 8
-// divide(10, 2); ---> 5
+add(1,9); 
+minus(5, 3);
+multiply(2,4); 
+divide(10, 2); 
